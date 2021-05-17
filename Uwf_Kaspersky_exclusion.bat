df67c86466978bc64cff168d0e5ba820c4208aaa
vijai <vijai.djearam@gmail.com>
2021-05-17 16:19:07 +0200

    Update Uwf_Kaspersky_exclusion.bat

--- Uwf_Kaspersky_exclusion.bat
+++ Uwf_Kaspersky_exclusion.bat
@@ -1,5 +1,4 @@
 uwfmgr registry add-exclusion "HKLM\SOFTWARE\WOW6432Node\KasperskyLab\protected\KES\environment\ProductHotfix"
-uwfmgr registry add-exclusion "HKLM\SOFTWARE\WOW6432Node\KasperskyLab\protected\KES\environment\ProductHotfix"
 uwfmgr registry add-exclusion "HKCU\Software\Classes\WOW6432Node\CLSID\{7e5fe3d9-985f-4908-91f9-ee19f9fd1514}\InprocHandler"
 uwfmgr registry add-exclusion "HKCR\WOW6432Node\CLSID\{7e5fe3d9-985f-4908-91f9-ee19f9fd1514}"
 uwfmgr registry add-exclusion "HKCU\Software\Classes\WOW6432Node\CLSID\{603D3800-BD81-11D0-A3A5-00C04FD706EC}"
