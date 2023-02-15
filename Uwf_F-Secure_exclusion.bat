b2fb00c8c904a1b6ab5b4f8cc99c0fd5a5fde7b5
vijai <vijai.djearam@gmail.com>
2023-02-15 13:00:33 +0100

    Update Uwf_F-Secure_exclusion.bat

--- Uwf_F-Secure_exclusion.bat
+++ Uwf_F-Secure_exclusion.bat
@@ -1,7 +1,7 @@
-uwfmgr registry add-exclusion "HKLM\SOFTWARE\F-Secure\"
-uwfmgr registry add-exclusion "HKLM\SOFTWARE\F-Secure_Debug"
-uwfmgr registry add-exclusion "HKLM\SOFTWARE\WOW6432Node\F-Secure\"
-uwfmgr file add-exclusion "C:\Program Files (x86)\F-Secure\"
-uwfmgr file add-exclusion "C:\ProgramData\F-Secure\"
-uwfmgr file add-exclusion "C:\Windows\System32\config\systemprofile\AppData\Roaming\f-secure\"
+uwfmgr registry add-exclusion "HKLM\SOFTWARE\F-Secure\*.*"
+uwfmgr registry add-exclusion "HKLM\SOFTWARE\F-Secure_Debug\*.*"
+uwfmgr registry add-exclusion "HKLM\SOFTWARE\WOW6432Node\F-Secure\*.*"
+uwfmgr file add-exclusion "C:\Program Files (x86)\F-Secure\*.*"
+uwfmgr file add-exclusion "C:\ProgramData\F-Secure\*.*"
+uwfmgr file add-exclusion "C:\Windows\System32\config\systemprofile\AppData\Roaming\f-secure\*.*"
 Pause
