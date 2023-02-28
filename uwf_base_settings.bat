e1cc795cfd25abdc8743159df83b83926071c3b1
vijai <vijai.djearam@gmail.com>
2023-02-28 10:48:34 +0100

    Update uwf_base_settings.bat

--- uwf_base_settings.bat
+++ uwf_base_settings.bat
@@ -1,6 +1,7 @@
-uwfmgr overlay set-size 4096
-uwfmgr overlay set-criticalthreshold 4000
-uwfmgr overlay set-warningthreshold 4000
+uwfmgr overlay set-type disk
+uwfmgr overlay set-size 20480
+uwfmgr overlay set-criticalthreshold 20000
+uwfmgr overlay set-warningthreshold 20000
 uwfmgr.exe volume protect c:
 uwfmgr.exe filter enable
 Pause
